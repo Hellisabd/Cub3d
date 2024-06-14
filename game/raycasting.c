bbd6562b43773e6535f7eb01cd92f697e6a732ff
Antoine Mirloup <antoine.mirloup@gmail.com>
2024-06-14 12:58:37 +0200

    main 2024-06-14 12:58:37 main 2024-06-14 12:58:37

--- game/raycasting.c
+++ game/raycasting.c
@@ -16,8 +16,10 @@ void	print_map(t_cub *cub)
 				printf("%s%c%s", RED, cub->map->map[i][j], NC);
 			else if (cub->map->map[i][j] == '0')
 				printf("%s%c%s", BLUE, cub->map->map[i][j], NC);
-			else
+			else if (cub->map->map[i][j] == 'P')
 				printf("%s%c%s", GREEN, cub->map->map[i][j], NC);
+			else 
+				printf("%s%c%s", YELLOW, cub->map->map[i][j], NC);
 			j++;
 		}
 		i++;
@@ -52,7 +54,10 @@ float	calc_ray_y(float dist_y, t_cub *cub)
 {
 	float	next;
 
-
+	// debug_float(GREEN, "dist_y", dist_y);
+	// if (cub->angle > 0)
+	// 	next = -dist_y / sin(cub->angle);
+	// else
 	next = dist_y / cos(cub->angle);
 	return (next);
 }
@@ -60,107 +65,114 @@ float	calc_ray_y(float dist_y, t_cub *cub)
 float	calc_ray_x(float dist_x, t_cub *cub)
 {
 	float	next;
-	
+
+	// debug_float(GREEN, "dist_x", dist_x);
 	next = dist_x / sin(cub->angle);
 	return (next);
 }
 
 void	raycasting(t_cub *cub)
 {
-	float		x;
-	float		y;
 	int			i;
 	int			j;
-	float dist_y;
-	float dist_x;
-	// bool		reset_y;
+	float		dist_y;
+	float		dist_x;
 	float		next_x;
 	float		next_y;
-	// int		reset_x;
 
 	set_player_pos(cub);
-	cub->fov = PI * 11 / 30;
+	cub->fov = PI * 66 / 180;
 	cub->angle = -cub->fov / 2;
-	// cub->angle = PI / 2;
+	cub->dir_x = -1;
+	cub->dir_y = 1;
 	while (cub->angle < cub->fov / 2)
 	{
-		// reset_y = false;
-		x = cub->p_x;
-		y = cub->p_y;
-		// cub->fov = 3.1416;
-		// printf("x: %f\n", x);
-		// printf("y: %f\n", y);
-		i = floor(y);
-		j = floor(x);
-		// printf("i: %i\n", i);
-		// printf("j: %i\n", j);
+		i = floor(cub->p_y);
+		j = floor(cub->p_x);
 		if (cub->dir_x < 0)
-			dist_x = x - fabs(floor(x));
+			dist_x = cub->p_x - fabs(floor(cub->p_x));
 		else
-			dist_x = x - fabs(ceil(x));
+			dist_x = cub->p_x - fabs(ceil(cub->p_x));
 		if (cub->dir_y < 0)
-			dist_y = y - fabs(floor(y));
+			dist_y = cub->p_y - fabs(floor(cub->p_y));
 		else
-			dist_y = y - fabs(ceil(y));
-		next_x =  calc_ray_x(dist_x, cub);
+			dist_y = cub->p_y - fabs(ceil(cub->p_y));
+		next_x =  calc_ray_x(dist_x, cub);		
 		next_y =  calc_ray_y(dist_y, cub);
-
 		// debug_float(YELLOW, "next_x :", next_x);
-		// debug_float(YELLOW, "next_y :", next_y);
-		// printf("next_y : %f\n", next_y);
-		// debug_float(RED, "angle :", cub->angle);
-		while (cub->map->map[i][j] == '0' || cub->map->map[i][j] == cub->map->d || cub->map->map[i][j] == 'P')
+		debug_float(RED, "angle :", cub->angle);
+		// printf("\n\n");
+		// printf("i : %d\nj : %d\n", i, j);
+		while (cub->map->map[i][j] == '0' || cub->map->map[i][j] == cub->map->d || cub->map->map[i][j] == 'P' || cub->map->map[i][j] == 'Q')
 		{
-			debug_float(RED, "next_y :", fabs(next_y));
-			debug_float(PURPLE, "y :", y);
-			debug_float(BLUE, "next_x :",fabs( next_x));
-			debug_float(GREEN, "x :", x);
-			if (next_y < 0 && fabs(next_x) >= fabs(next_y))
+			
+			if (dist_y < 0 && fabs(next_x) >= fabs(next_y))
 			{
-				cub->map->map[i][j] = 'P';
-				// printf("i: %d | j: %d\n", i , j);
+				if (cub->angle < 0)
+					cub->map->map[i][j] = 'P';
+				else
+					cub->map->map[i][j] = 'Q';
 				i--;
 				dist_y -= 1;
 				next_y =  calc_ray_y(dist_y, cub);
-				// y += next_y;
 			}
-			else if (next_y > 0 && fabs(next_x) >= fabs(next_y))
+			else if (dist_y > 0 && fabs(next_x) >= fabs(next_y))
 			{
-				cub->map->map[i][j] = 'P';
-				// printf("i: %d | j: %d\n", i , j);
+				if (cub->angle < 0)
+					cub->map->map[i][j] = 'P';
+				else
+					cub->map->map[i][j] = 'Q';
 				i++;
 				dist_y += 1;
 				next_y =  calc_ray_y(dist_y, cub);
-				// y += next_y;
 			}
-			else if (next_x < 0 && fabs(next_y) >= fabs(next_x))
+			else if (dist_x < 0 && fabs(next_y) >= fabs(next_x))
 			{
-				cub->map->map[i][j] = 'P';
-				// printf("i: %d | j: %d\n", i , j);
-				j--;
-				// x += next_x;
-				dist_x -= 1;
-				next_x =  calc_ray_x(dist_x, cub);
+				if (cub->angle < 0)
+					cub->map->map[i][j] = 'P';
+				else
+					cub->map->map[i][j] = 'Q';
+				// printf("inf_x\n");
+				// debug_float(BLUE, "next_x :", next_x);
+				if (cub->angle < 0)
+				{
+					j--;
+					dist_x -= 1;
+					next_x =  calc_ray_x(dist_x, cub);
+				}
+				else
+				{
+					j++;
+					dist_x -= 1;
+					next_x =  -calc_ray_x(dist_x, cub);
+				}
+				// debug_float(BLUE, "next_x :", next_x);
 			}
-			else if (next_x > 0 && fabs(next_y) >= fabs(next_x))
+			else if (dist_x > 0 && fabs(next_y) >= fabs(next_x))
 			{
-				cub->map->map[i][j] = 'P';
-				// printf("i: %d | j: %d\n", i , j);
-				j++;
-				dist_y += 1;
-				next_y =  calc_ray_x(dist_x, cub);
-				// x += next_x;
+				if (cub->angle < 0)
+					cub->map->map[i][j] = 'P';
+				else
+					cub->map->map[i][j] = 'Q';
+				// printf("sup_x\n");
+				// debug_float(RED, "before calc : next_x :", next_x);
+				if (cub->angle < 0)
+				{
+					j++;
+					dist_x += 1;
+					next_x =  calc_ray_x(dist_x, cub);
+				}
+				else
+				{
+					j--;
+					dist_x += 1;
+					next_x =  -calc_ray_x(dist_x, cub);
+				}
+
+				// debug_float(RED, "after calc : next_x :", next_x);
 			}
-			// if (fabs(next_x) >= fabs(next_y))
-			// 	x += next_x;
-			// else if (fabs(next_y) >= fabs(next_x))
-			// 	y += next_y;
-			// debug_str(YELLOW, NULL, "JE PASSE");
 		}
-		// printf("x: %f\n", x);
-		// printf("y: %f\n", y);
-		cub->angle += PI / 200;
+		cub->angle += cub->fov / 66;
 	}
-	// printf("dir_y %f\ndir_x : %f\n", cub->dir_y, cub->dir_x);
 	print_map(cub);
 }
